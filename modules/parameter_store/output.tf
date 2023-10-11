04094524f5d37ffc5511bc85fca1a9bded4fb256
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-10-12 00:30:33 +0200

    fixed typo in outputs

--- modules/parameter_store/output.tf
+++ modules/parameter_store/output.tf
@@ -1,5 +1,5 @@
-output "cluster_name" { aws_ssm_parameter.cluster_name.value }
-output "docker_credentials" { aws_ssm_parameter.docker_credentials.value }
-output "access_key" { aws_ssm_parameter.access_key.value }
-output "secret_key" { aws_ssm_parameter.secret_key.value }
-output "region" { aws_ssm_parameter.region.value }
+output "cluster_name" { value = aws_ssm_parameter.cluster_name.value }
+output "docker_credentials" { value = aws_ssm_parameter.docker_credentials.value }
+output "access_key" { value = aws_ssm_parameter.access_key.value }
+output "secret_key" { value = aws_ssm_parameter.secret_key.value }
+output "region" { value = aws_ssm_parameter.region.value }
