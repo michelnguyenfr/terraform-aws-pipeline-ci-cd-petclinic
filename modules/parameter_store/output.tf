ae2ddbcbd1c54b64f9a533c84d3949ff918bd88f
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-10-12 00:29:38 +0200

    fix outputs for parameter store

--- modules/parameter_store/output.tf
+++ modules/parameter_store/output.tf
@@ -1,5 +1,5 @@
-output "cluster_name" { aws_ssm_parameter.cluster_name }
-output "docker_credentials" { aws_ssm_parameter.docker_credentials }
-output "access_key" { aws_ssm_parameter.access_key }
-output "secret_key" { aws_ssm_parameter.secret_key }
-output "region" { aws_ssm_parameter.region }
+output "cluster_name" { aws_ssm_parameter.cluster_name.value }
+output "docker_credentials" { aws_ssm_parameter.docker_credentials.value }
+output "access_key" { aws_ssm_parameter.access_key.value }
+output "secret_key" { aws_ssm_parameter.secret_key.value }
+output "region" { aws_ssm_parameter.region.value }
