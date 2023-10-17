2a79ff24339c1d7aeec51b1905d4b1ee1d5e5217
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-10-17 16:21:27 +0200

    sharing bucket name

--- modules/codepipeline/main.tf
+++ modules/codepipeline/main.tf
@@ -5,7 +5,7 @@ resource "aws_codepipeline" "pipeline" {
   role_arn = var.pipeline_role_arn  # Replace with your CodePipeline role ARN
 
   artifact_store {
-    location = aws_s3_bucket.dummy_bucket  # Replace with your S3 bucket name
+    location = aws_s3_bucket.dummy_bucket.id  # Replace with your S3 bucket name
     type     = "S3"
   }
 
@@ -99,7 +99,8 @@ resource "aws_codepipeline" "pipeline" {
 
 resource "aws_s3_bucket" "dummy_bucket" {
   bucket = "petclinic_s3_bucket"
-
+  force_destroy = true
+  
   tags = {
     Name        = "petclinic_b3_bucket"
   }
