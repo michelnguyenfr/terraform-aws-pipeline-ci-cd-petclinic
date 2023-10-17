9294df62e0dda0f05623a5b59b99e25da7d200a5
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-10-17 14:24:52 +0200

    import ssm parameters before attempt to create

--- modules/parameter_store/main.tf
+++ modules/parameter_store/main.tf
@@ -1,7 +1,18 @@
+import {
+  to = aws_ssm_parameter.cluster_name
+  id = "PETCLINIC_EKS_CLUSTER_NAME"
+}
+
 resource "aws_ssm_parameter" "cluster_name" {
     name  = "PETCLINIC_EKS_CLUSTER_NAME"
     type  = "String"
     value = var.cluster_name
+}
+
+
+import {
+  to = aws_ssm_parameter.docker_credentials
+  id = "DOCKER_CREDENTIALS"
 }
 
 resource "aws_ssm_parameter" "docker_credentials" {
@@ -10,16 +21,31 @@ resource "aws_ssm_parameter" "docker_credentials" {
     value = var.docker_credentials
 }
 
+import {
+  to = aws_ssm_parameter.access_key
+  id = "DOCKER_CREDENTIALS"
+}
+
 resource "aws_ssm_parameter" "access_key" {
     name  = "ACCESS_KEY_ID"
     type  = "SecureString"
     value = var.access_key
 }
 
+import {
+  to = aws_ssm_parameter.secret_key
+  id = "SECRET_ACCESS_KEY"
+}
+
 resource "aws_ssm_parameter" "secret_key" {
     name  = "SECRET_ACCESS_KEY"
     type  = "SecureString"
     value = var.secret_key
+}
+
+import {
+  to = aws_ssm_parameter.region
+  id = "PETCLINIC_REGION"
 }
 
 resource "aws_ssm_parameter" "region" {
