906e1d34da39c3583fbc21d0e4e5f5c720a7a161
michelnguyenfr <michel.nguyen.fr@gmail.com>
2023-10-17 14:34:16 +0200

    adding overwrites

--- modules/parameter_store/main.tf
+++ modules/parameter_store/main.tf
@@ -2,28 +2,33 @@ resource "aws_ssm_parameter" "cluster_name" {
     name  = "PETCLINIC_EKS_CLUSTER_NAME"
     type  = "String"
     value = var.cluster_name
+    overwrite = true
 }
 
 resource "aws_ssm_parameter" "docker_credentials" {
     name  = "DOCKER_CREDENTIALS"
     type  = "SecureString"
     value = var.docker_credentials
+    overwrite = true
 }
 
 resource "aws_ssm_parameter" "access_key" {
     name  = "ACCESS_KEY_ID"
     type  = "SecureString"
     value = var.access_key
+    overwrite = true
 }
 
 resource "aws_ssm_parameter" "secret_key" {
     name  = "SECRET_ACCESS_KEY"
     type  = "SecureString"
     value = var.secret_key
+    overwrite = true
 }
 
 resource "aws_ssm_parameter" "region" {
     name  = "PETCLINIC_REGION"
     type  = "String"
     value = var.region
+    overwrite = true
 }
